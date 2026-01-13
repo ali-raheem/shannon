673966187ce53753f418f8bc51e4141c352013f1
Ali Raheem <github@shoryuken.me>
2026-01-13 22:59:46 +0000

    Fix panic on small values of width or height

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
 use std::fs::File;
-use std::io::prelude::*;
 use std::io::BufReader;
+use std::io::prelude::*;
+use std::process;
 use textplots::{Chart, Plot, Shape};
 
 use shannon::entropy;
@@ -22,25 +23,34 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-
+    if args.width < 32 || args.height < 32 {
+        println!("Width and Height must be atleast 32.");
+        process::exit(1);
+    }
     let f = File::open(&args.input_file).expect("Could not open input file");
     let mut reader = BufReader::new(f);
 
-    let mut read_buffer = vec!(0u8; args.block_size);
+    let mut read_buffer = vec![0u8; args.block_size];
     let mut s = Vec::new();
     loop {
-	let len = reader.read(&mut read_buffer).expect("Could not read file");
-	if len == 0 {
-	    break;
-	}
-	s.push(entropy::<f32>(&read_buffer[..len]));
+        let len = reader.read(&mut read_buffer).expect("Could not read file");
+        if len == 0 {
+            break;
+        }
+        s.push(entropy::<f32>(&read_buffer[..len]));
     }
     let x_max = s.len() as f32;
-    let y_max = args.y_max.unwrap_or_else(|| s.iter().fold(0.0_f32, |a, &y| a.max(y)));
+    let y_max = args
+        .y_max
+        .unwrap_or_else(|| s.iter().fold(0.0_f32, |a, &y| a.max(y)));
 
-    let s: Vec<(f32, f32)> = s.into_iter().enumerate().map(|(x, y)| (x as f32, y)).collect();
+    let s: Vec<(f32, f32)> = s
+        .into_iter()
+        .enumerate()
+        .map(|(x, y)| (x as f32, y))
+        .collect();
 
     Chart::new_with_y_range(args.width, args.height, 0.0, x_max, 0.0, y_max)
-	.lineplot(&Shape::Bars(&s))
-	.display();
+        .lineplot(&Shape::Bars(&s))
+        .display();
 }
